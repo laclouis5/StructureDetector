20f21cbaf62d9a5def8ffae302af7af3b7095b05
Louis Lac <lac.louis5@gmail.com>
2021-04-26 20:06:58 +0200

    Small fix

--- sources/library/model/loss.py
+++ sources/library/model/loss.py
@@ -47,9 +47,8 @@ class L1Loss(nn.Module):
     # input: (B, 2, H, W), target: (B, K, 2), inds & mask: (B, K)
     def forward(self, input, target, inds, mask):
         preds = transpose_and_gather(input, inds)  # (B, K, 2)
-        mask = mask.unsqueeze(2).expand_as(preds).float()  # (B, K, 2)
-        loss = F.l1_loss(preds * mask, target * mask, reduction="sum")  # (1)
-        return loss / (mask.sum() + 1e-7)  # (1)
+        loss = torch.abs((preds - target) * mask[..., None]).sum()  # (1)
+        return loss / (mask.sum() + 1e-7)
 
 
 class SmoothL1Loss(nn.Module):
