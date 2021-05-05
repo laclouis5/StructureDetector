2253d061e9e19d750c965b98b2b42fc9db43359b
Louis Lac <lac.louis5@gmail.com>
2021-05-05 14:13:36 +0200

    HourglassNet

--- sources/library/model/network.py
+++ sources/library/model/network.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torchvision
+from mmdet.models import HourglassNet
 
 
 class Fpn(nn.Module):
@@ -40,37 +41,11 @@ class Network(nn.Module):
         self.out_channels = self.label_count + self.part_count + 4  # M+N+4
         self.fpn_depth = args.fpn_depth
 
-        resnet = torchvision.models.resnet34(pretrained=pretrained)
-
-        self.adpater = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)  # /4
-
-        self.down1 = resnet.layer1  # /1 -> /4
-        self.down2 = resnet.layer2  # /2 -> /8
-        self.down3 = resnet.layer3  # /2 -> /16
-        self.down4 = resnet.layer4  # /2 -> /32
-
-        self.up1 = nn.Conv2d(512, self.fpn_depth, kernel_size=1)  # x1 -> /32
-        self.up2 = Fpn(256, self.fpn_depth)  # x2 -> /16
-        self.up3 = Fpn(128, self.fpn_depth)  # x2 -> /8
-        self.up4 = Fpn(64, self.fpn_depth)  # x2 -> /4
-
+        self.hourglass = HourglassNet(num_stacks=1, feat_channel=args.fpn_depth)
         self.head = Head(self.fpn_depth, self.out_channels)
 
     def forward(self, x):  # (B, 3, H, W)
-        p1 = self.adpater(x)  # (B, 64, H/4, W/4)
-
-        p2 = self.down1(p1)  # (B, 64, H/4, W/4)
-        p3 = self.down2(p2)  # (B, 128, H/8, W/8)
-        p4 = self.down3(p3)  # (B, 256, H/16, W/16)
-        p5 = self.down4(p4)  # (B, 512, H/32, W/32)
-
-        f4 = self.up1(p5)  # (B, 128, H/32, W/32)
-        f3 = self.up2(f4, p4)  # (B, 128, H/16, W/16)
-        f2 = self.up3(f3, p3)  # (B, 128, H/8, W/8)
-        f1 = self.up4(f2, p2)  # (B, 128, H/4, W/4)
-
-        out = self.head(f1)  # (B, M+N+4, H/4, W/4)
-
+        out = self.head(self.hourglass(x)[0])  # (B, M+N+4, H/4, W/4)
         nb_hm = self.label_count + self.part_count  # M+N
 
         return {  # R = 4
