3cd4f7e0a9c0c9e18cab382a9342d25022ae064a
Louis Lac <lac.louis5@gmail.com>
2021-05-21 18:16:07 +0200

    Fix

--- sources/evaluate.py
+++ sources/evaluate.py
@@ -20,8 +20,7 @@ if __name__ == "__main__":
     decoder = Decoder(args)
     net = Network(args)
     net.load_state_dict(torch.load(args.pretrained_model, map_location=args.device))
-    net = net.to(args.device)
-    net.eval()
+    net = net.eval().to(args.device)
 
     for batch in tqdm(dataloader, desc="Evaluation", unit="image"):
         for k, v in batch.items():
