cbb112c487581c456e8283b480071a642e086fb9
Lac Louis <lac.louis5@gmail.com>
2021-06-10 15:43:32 +0200

    Confusion matrix for classification

--- src/library/model/evaluator.py
+++ src/library/model/evaluator.py
@@ -4,34 +4,39 @@ import sys
 from functools import reduce
 from rich import print as rprint
 from rich.table import Table, Column
+from collections import defaultdict
 
 
 class Evaluation:
 
-    def __init__(self, tp=0, npos=0, ndet=0, acc=None):
+    def __init__(self, tp=0, npos=0, ndet=0, acc=None, counts=None):
         self.tp = tp
         self.npos = npos  
         self.ndet = ndet
         self.acc = acc or []
+        self.count_errors = counts or []
 
     def reset(self):
         self.tp = 0
         self.npos = 0  
         self.ndet = 0
         self.acc = []
+        self.count_errors = []
 
     def __add__(self, other):
         return Evaluation(
             self.tp + other.tp,
             self.npos + other.npos,
             self.ndet + other.ndet,
-            self.acc + other.acc)
+            self.acc + other.acc,
+            self.count_errors + other.count_errors)
 
     def __iadd__(self, other):
         self.tp += other.tp
         self.npos += other.npos
         self.ndet += other.ndet
         self.acc += other.acc
+        self.count_errors += other.count_errors
         return self
 
     @property
@@ -68,12 +73,16 @@ class Evaluation:
     def acc_err(self):
         return np.std(self.acc) / np.sqrt(len(self.acc)) if len(self.acc) != 0 else float("nan")
 
+    # @property
+    # def avg_count_error(self):
+    #     return np.mean(self.count_errors) if len(self.count_errors) != 0 else float("nan")
+
     def stats(self):
         return f"{self.npos}", f"{self.ndet}", f"{self.recall:.2%}", f"{self.precision:.2%}", f"{self.f1_score:.2%}", f"{self.avg_acc:.2%}", f"{self.acc_err:.2%}"
 
     @staticmethod
     def columns():
-        return (Column("Gts.", justify="right"), Column("Preds.", justify="right"), Column("Rec.", justify="right"), Column("Prec.", justify="right"), Column("F1 Score", justify="right", style="green"), Column("L. Acc.", justify="right"), Column("L. Err.", justify="right"))
+        return Column("Gts.", justify="right"), Column("Preds.", justify="right"), Column("Rec.", justify="right"), Column("Prec.", justify="right"), Column("F1 Score", justify="right", style="green"), Column("L. Acc.", justify="right"), Column("L. Err.", justify="right")
 
     def __repr__(self):
         return f"f1: {self.f1_score:.2%}, rec: {self.recall:.2%}, prec: {self.precision:.2%}, npos: {self.npos}, ndet: {self.ndet}, tp/fp/fn: {self.tp}/{self.fp}/{self.fn}, avg_acc: {self.avg_acc:.2}"
@@ -82,6 +91,12 @@ class Evaluation:
         table = Table(*Evaluation.columns())
         table.add_row(*self.stats())
         rprint(table)
+
+    def save_conf_matrix(self):
+        conf_mat = np.zeros((10, 10))
+        for p, e in self.count_errors:
+            conf_mat[e, p] += 1
+        np.save("conf_mat.npy", conf_mat)
 
 
 class Evaluations:
@@ -334,7 +349,7 @@ class Evaluator:
             (self.args.width, self.args.height),
             img_size)
 
-        (img_w, img_h) = img_size
+        img_w, img_h = img_size
         dist_thresh = min(img_w, img_h) * self.args.dist_threshold
 
         preds = dict_grouping(prediction.objects, key=lambda obj: f"{obj.name}_{obj.nb_parts}")
@@ -357,7 +372,7 @@ class Evaluator:
                 best_dist = sys.float_info.max
                 idx_best = None
 
-                for (i, gt) in enumerate(gts_label):
+                for i, gt in enumerate(gts_label):
                     dist = pred.distance(gt)
                     if dist < best_dist:
                         best_dist = dist
@@ -367,6 +382,59 @@ class Evaluator:
                     visited[idx_best] = True
                     res.tp += 1
                     res.acc.append(best_dist / min(img_w, img_h))
+
+        return result
+
+    def eval_classif_2(self, prediction, annotation):
+        img_size = annotation.img_size
+        annotation = annotation.resized(
+            (self.args.width, self.args.height),
+            img_size)
+        prediction = prediction.resized(
+            (self.args.width, self.args.height),
+            img_size)
+
+        img_w, img_h = img_size
+        dist_thresh = min(img_w, img_h) * self.args.dist_threshold
+
+        preds = dict_grouping(prediction.objects, key=lambda obj: f"{obj.name}_{obj.nb_parts}")
+        _gts = dict_grouping(annotation.objects, key=lambda obj: f"{obj.name}_{obj.nb_parts}")
+        gts = annotation.objects
+        labels = Evaluator.get_classification_labels()
+        result = Evaluations(labels)
+
+        for label in labels:
+            res = result[label]
+            preds_label = preds.get(label, [])
+            gts_label = _gts.get(label, [])
+
+            res.ndet = len(preds_label)
+            res.npos = len(gts_label)
+
+            preds_label = sorted(preds_label, key=lambda obj: obj.anchor.score, reverse=True)
+            visited = np.repeat(False, len(gts))
+
+            for pred in preds_label:
+                best_dist = sys.float_info.max
+                idx_best = None
+
+                for i, gt in enumerate(gts):
+                    dist = pred.distance(gt)
+                    if dist < best_dist:
+                        best_dist = dist
+                        idx_best = i
+
+                if best_dist > dist_thresh or visited[idx_best] or (pred.name not in gts[idx_best].name):
+                    continue
+                
+                if pred.nb_parts != gts[idx_best].nb_parts:
+                    res.count_errors.append((pred.nb_parts, gts[idx_best].nb_parts))
+                    continue
+
+                visited[idx_best] = True
+                res.tp += 1
+                res.acc.append(best_dist / min(img_w, img_h))
+                res.count_errors.append((pred.nb_parts, gts[idx_best].nb_parts))
 
         return result
 
