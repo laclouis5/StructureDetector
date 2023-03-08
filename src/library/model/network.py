a00cca7d183acdbf116bf733cbb38542708230d6
Louis Lac <lac.louis5@gmail.com>
2023-03-08 15:57:14 +0100

    Removed biases before BNs

--- src/library/model/network.py
+++ src/library/model/network.py
@@ -8,10 +8,10 @@ class DoubleConv(nn.Module):
         super().__init__()
 
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 3, padding=1),
+            nn.Conv2d(in_ch, out_ch, 3, padding=1, bias=False),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, 3, padding=1),
+            nn.Conv2d(out_ch, out_ch, 3, padding=1, bias=False),
             nn.BatchNorm2d(out_ch),
             nn.ReLU(inplace=True),
         )
